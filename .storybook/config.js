0704441d1256c113741abeda7e1d9e6bf1babb19
EmaSuriano <emanuel.suriano@gmail.com>
2018-10-19 00:31:08 +0200

    Add percy integration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,13 @@
-import { configure } from '@storybook/react';
+import { configure, getStorybook, setAddon } from '@storybook/react';
+import createPercyAddon from '@percy-io/percy-storybook';
 
-// automatically import all files ending in *.stories.js
+const { percyAddon, serializeStories } = createPercyAddon();
+setAddon(percyAddon);
+
 const req = require.context('../stories', true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
+serializeStories(getStorybook);
